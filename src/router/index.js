e495ae715ae33126df0db67f9961c766db96e392
pedrocheetoz <Schetzpedro297@gmail.com>
2026-09-24 22:13:24 -0300

    Fiz algumas coisinhas

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import PetsView from '@/views/PetsView.vue';
+import NovoPets from '@/views/NovoPets.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +10,20 @@ const router = createRouter({
       name: 'home',
       component: () => import('../views/HomeView.vue'),
     },
+    {
+      path: '/pets',
+      name: 'PetsView',
+      component: PetsView
+
+    },
+    {
+      path: '/pets/novo',
+      name: 'NovoPets',
+      component: NovoPets
+    },
   ],
 });
+
+
 
 export default router;
---
+++ src/views/NovoPets.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Oi</h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
